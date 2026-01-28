60f251678ebea1462d2548c5a197202dd9c619c8
TBSKBJustin <tbskbjustin@gmail.com>
2026-01-27 17:07:30 -0700

    npm

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Church-Web-Template/',
+  base: process.env.NODE_ENV === 'production' ? '/Church-Web-Template/' : '/',
   build: {
     outDir: 'docs',
   },
